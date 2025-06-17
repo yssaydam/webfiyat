939985297c405908270f5eeaeec0e726664143fe
Yavuz Selim SAYDAM <yavuzselimsaydam@MacBookPro.home>
2025-06-17 11:53:15 +0300

    new

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('sezgin-cache').then(function (cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/fiyatlar.json',
+                '/favicon/favicon-96x96.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
